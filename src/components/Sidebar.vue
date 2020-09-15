3bfce67986908b4df13c1d16c21ea32d24dc02f6
Travis Reynolds <travis@cirruscreative.co.uk>
2020-09-15 00:24:04 +0000

    Update to add checkout emthod

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -6,43 +6,57 @@
     overlay
     button
     @close="closeSidebar">
-    <SfHeading
-      v-if="!cart.length"
-      :level="4"
-      class="sf-heading--left"
-      title="Empty..."
-      subtitle="There is nothing here yet - try searching for some products with the searchbar." />
-    <SfCollectedProduct
-      v-for="item in cart"
-      :key="item.id"
-      :qty="item.qty"
-      :image="item.image.thumbnail"
-      :image-width="150"
-      :image-height="150"
-      :title="item.productTitle"
-      :regular-price="item.total"
-      @input="updateItemQty(item, $event)"
-      @click:remove="removeFromCart(item)">
-      <template #configuration>
-        <div :style="{ margin: '1rem 0 0 0' }">
-          <SfProperty
-            v-for="(option, i) in item.options"
-            :key="i"
-            :name="option.name"
-            :value="option.value" />
-        </div>
-      </template>
-    </SfCollectedProduct>
+    <template #default>
+      <SfHeading
+        v-if="!cart.length"
+        :level="4"
+        class="sf-heading--left"
+        title="Empty..."
+        subtitle="There is nothing here yet - try searching for some products with the searchbar." />
+      <SfCollectedProduct
+        v-for="item in cart"
+        :key="item.id"
+        :qty="item.qty"
+        :image="item.image.thumbnail"
+        :image-width="150"
+        :image-height="150"
+        :title="item.productTitle"
+        :regular-price="item.total"
+        @input="updateItemQty(item, $event)"
+        @click:remove="removeFromCart(item)">
+        <template #configuration>
+          <div :style="{ margin: '1rem 0 0 0' }">
+            <SfProperty
+              v-for="(option, i) in item.options"
+              :key="i"
+              :name="option.name"
+              :value="option.value" />
+          </div>
+        </template>
+      </SfCollectedProduct>
+    </template>
+    <template #content-bottom>
+      <SfButton
+        class="sf-button--full-width sf-button--outline"
+        :disabled="isLoading"
+        @click="checkout">
+        Checkout
+      </SfButton>
+    </template>
   </SfSidebar>
 </template>
 
 <script>
 // Components
-import { SfSidebar, SfHeading, SfCollectedProduct, SfProperty } from '@storefront-ui/vue'
+import { SfSidebar, SfHeading, SfCollectedProduct, SfProperty, SfButton } from '@storefront-ui/vue'
+
+// GraphQL
+import { CheckoutCreateMutation } from '@/graphql/checkout'
 
 export default {
   name: 'CartSidebar',
-  components: { SfSidebar, SfHeading, SfCollectedProduct, SfProperty },
+  components: { SfSidebar, SfHeading, SfCollectedProduct, SfProperty, SfButton },
+  data: () => ({ isLoading: false }),
   computed: {
     isVisible () { return this.$store.getters.isSidebarVisible },
     cart () { return this.$store.getters.cart }
@@ -56,6 +70,34 @@ export default {
     },
     removeFromCart ({ variantId }) {
       this.$store.dispatch('removeFromCart', variantId)
+    },
+    async checkout () {
+      if (!this.cart.length) return alert('No items in cart')
+      this.isLoading = true
+
+      const lineItems = this.cart.map(item => ({ quantity: item.qty, variantId: item.variantId }))
+      const checkoutInput = { lineItems }
+
+      try {
+        const { checkoutCreate } = await this.$graphql.request(CheckoutCreateMutation, { input: checkoutInput })
+        const { checkout, checkoutUserErrors } = checkoutCreate
+
+        if (checkoutUserErrors.length) {
+          const [{ message }] = checkoutUserErrors
+          throw new Error(message)
+        }
+
+        window.location = checkout.webUrl
+      } catch (error) {
+        console.error(error)
+        this.isLoading = false
+
+        this.$notify({
+          title: 'Whoops...',
+          type: 'danger',
+          message: 'Something went wrong - please try again.'
+        })
+      }
     }
   }
 }
